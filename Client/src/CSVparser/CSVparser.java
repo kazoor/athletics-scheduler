baba395bdb04827026826fb60325728df7225445
samson <xsates25@hotmail.com>
2022-05-12 01:50:54 +0200

    Update CSVparser.java

--- Client/src/CSVparser/CSVparser.java
+++ Client/src/CSVparser/CSVparser.java
@@ -3,19 +3,19 @@ package CSVparser;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVparser
 {
+    public static String p = "src/Exemple.csv";
     public static void parser(String path) throws IOException, CsvException {
          String[] storing;
-        try(CSVReader reader = new CSVReader(new FileReader("src/Exemple.csv"))) {
+        try(CSVReader reader = new CSVReader(new FileReader(path))) {
             while ((storing = reader.readNext()) != null){
                 System.out.println(storing[0]+storing[1]+storing[2]);
             }
         }
     }
-    public static void main(String args[]) throws IOException, CsvException {parser("hello");}
+    public static void main(String args[]) throws IOException, CsvException {parser(p);}
 }
