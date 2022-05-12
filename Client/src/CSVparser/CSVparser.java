e7c711590fcefddecd3fa504bc583ee5ef55eff7
samson <xsates25@hotmail.com>
2022-05-12 23:20:39 +0200

    parser done

--- Client/src/CSVparser/CSVparser.java
+++ Client/src/CSVparser/CSVparser.java
@@ -6,16 +6,22 @@ import com.opencsv.exceptions.CsvException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class CSVparser
-{
-    public static String p = "src/Exemple.csv";
-    public static void parser(String path) throws IOException, CsvException {
-         String[] storing;
-        try(CSVReader reader = new CSVReader(new FileReader(path))) {
-            while ((storing = reader.readNext()) != null){
-                System.out.println(storing[0]+storing[1]+storing[2]);
+
+public class CSVparser {
+    public static void parser() throws IOException, CsvException {
+        String[] storing;
+        try (CSVReader reader = new CSVReader(new FileReader("Client/src/Exemple.csv"))) {
+            while ((storing = reader.readNext()) != null) {
+                System.out.println(storing[0] + storing[1] + storing[2]);
             }
         }
     }
-    public static void main(String args[]) throws IOException, CsvException {parser(p);}
+
+  void run(){
+        try {
+            parser();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+  }
 }
--- Client/src/Client/Client.java
+++ Client/src/Client/Client.java
@@ -1,6 +1,12 @@
 package Client;
+import CSVparser.CSVparser;
+import com.opencsv.exceptions.CsvException;
+import java.io.IOException;
 
 public class Client {
-    // Client entrypoint.
-    public static void main(String args[]) {System.out.println("Client running!");}
+    public static void main(String[] args) throws IOException, CsvException {
+        //String fileName = "Client/src/Exemple.csv";
+        System.out.println("Client running!");
+        CSVparser.parser();
+    }
 }
